09066f54ce8a33417f4d85ed0c0199dcd62dbff5
Detrena <detrena@mail.ru>
2019-12-22 11:43:02 +0700

    Service Container update. Key dependencies - factories added

--- src/Detrena/BitrixModuleCore/Service/Container.php
+++ src/Detrena/BitrixModuleCore/Service/Container.php
@@ -8,11 +8,13 @@ use Detrena\BitrixModuleCore\Exceptions\ServiceNotFoundException;
 
 class Container implements ContainerInterface {
     protected $services = [];
+    protected $factories = [];
     protected $cache = [];
 
     public function __construct($params = [])
     {
         $this->services = $params;
+        $this->factories = $params['dependencies']['factories'];
     }
 
     /**
@@ -27,58 +29,81 @@ class Container implements ContainerInterface {
         if (array_key_exists($id, $this->cache))
             return $this->cache[$id];
 
+        if (array_key_exists($id, $this->factories)) {
+            $this->cache[$id] = $this->doFactory($id);
+            return $this->cache[$id];
+        }
+
         if (!array_key_exists($id, $this->services)) {
-            if (!class_exists($id))
-                throw new ServiceNotFoundException('Unknown service "' . $id . '"');
-
-            $reflection = new \ReflectionClass($id);
-            $arguments = [];
-            if (($constructor = $reflection->getConstructor()) !== null) {
-                foreach ($constructor->getParameters() as $parameter) {
-                    if ($paramClass = $parameter->getClass()) {
-                        $arguments[] = $this->get($paramClass->getName());
-                    } elseif ($parameter->isArray()) {
-                        $arguments[] = [];
-                    } else {
-                        if (!$parameter->isDefaultValueAvailable())
-                            throw new ServiceNotFoundException('Unable to resolve "' . $parameter->getName() . '" in service "' . $id . '"');
-
-                        $arguments[] = $parameter->getDefaultValue();
-                    }
-                }
-            }
-            $this->cache[$id] = $reflection->newInstanceArgs($arguments);
+            $this->cache[$id] = $this->doReflection($id);
             return $this->cache[$id];
-        }
-
-        if ($this->services[$id] instanceof \Closure) {
-            $this->cache[$id] = $this->services[$id]($this);
-
-            return $this->cache[$id];
-        }
-
-        // Factory class check
-        try {
-            $reflection = new \ReflectionClass($this->services[$id]);
-
-            /** @var callable $factory */
-            $factory = $reflection->newInstance();
-            $this->cache[$id] = call_user_func($factory, $this);
-
-            return $this->cache[$id];
-
-        } catch (\ReflectionException $e) {
-            // this is not a class
-
         }
 
         $this->cache[$id] = $this->services[$id];
-
         return $this->cache[$id];
     }
 
     public function has($id)
     {
         return array_key_exists($id, $this->services) || class_exists($id);
+    }
+
+
+    /**
+     * @param string $id
+     * @return mixed
+     * @throws ServiceFactoryNotCallableException
+     */
+    protected function doFactory($id)
+    {
+        if (is_array($this->factories[$id]) && is_callable($this->factories[$id]))
+            return $this->factories[$id]($this);
+
+        if (is_callable($this->factories[$id]))
+            return $this->factories[$id]($this);
+
+        try {
+            $reflection = new \ReflectionClass($this->factories[$id]);
+
+            /** @var callable $factory */
+            $factory = $reflection->newInstance();
+            return call_user_func($factory, $this);
+
+        } catch (\ReflectionException $e) {
+            // ID is not a class
+            throw new ServiceFactoryNotCallableException("Factory '".var_export($this->factories[$id],true)."' is not found or not callable!");
+        }
+    }
+
+    /**
+     * @param $id
+     * @return object
+     * @throws ServiceFactoryNotCallableException
+     * @throws ServiceNotFoundException
+     * @throws \ReflectionException
+     */
+    protected function doReflection($id)
+    {
+        if (!class_exists($id))
+            throw new ServiceNotFoundException('Unknown service "' . $id . '"');
+
+        $reflection = new \ReflectionClass($id);
+        $arguments = [];
+        if (($constructor = $reflection->getConstructor()) !== null) {
+            foreach ($constructor->getParameters() as $parameter) {
+                if ($paramClass = $parameter->getClass()) {
+                    $arguments[] = $this->get($paramClass->getName());
+                } elseif ($parameter->isArray()) {
+                    $arguments[] = [];
+                } else {
+                    if (!$parameter->isDefaultValueAvailable())
+                        throw new ServiceNotFoundException('Unable to resolve "' . $parameter->getName() . '" in service "' . $id . '"');
+
+                    $arguments[] = $parameter->getDefaultValue();
+                }
+            }
+        }
+
+        return $reflection->newInstanceArgs($arguments);
     }
 }
--- tests/DetrenaTest/config.php
+++ tests/DetrenaTest/config.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
-    \Detrena\BitrixModuleCore\Config\Options::class => \Detrena\BitrixModuleCore\Config\OptionsFactory::class,
-    \Psr\Log\LoggerInterface::class => \Detrena\BitrixModuleCore\Log\LoggerFactory::class,
+    'dependencies' => [
+        'factories' => [
+            \Detrena\BitrixModuleCore\Config\Options::class => \Detrena\BitrixModuleCore\Config\OptionsFactory::class,
+            \Psr\Log\LoggerInterface::class => \Detrena\BitrixModuleCore\Log\LoggerFactory::class,
+        ]
+    ],
     'log_file' => dirname(__DIR__) . '/logs/log',
     'cache_config' => false,
 ];
